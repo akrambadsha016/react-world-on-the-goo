535fac3a3dbd4d1206d14f45d0847b9e313e1693
akrambadsha016 <akrambadsha016@gmail.com>
2026-03-27 09:18:41 +0600

    added a country flag and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 
+import { Suspense } from 'react'
 import './App.css'
 import Countries from './components/Countries'
+
+
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+.then(res => res.json())
 
 function App() {
   return (
     <>
-      <Countries></Countries>
+      <Suspense fallback={<p>Nadir vai loading....</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
     </>
   )
 }
